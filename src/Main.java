c950fe7a96002973ee4f65949bfb8cdb77611c0d
jeison2125 <jeisonmafla2503@gmail.com>
2026-02-13 09:58:47 +0100

    Implementada función resta

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
 public static void main(String[] args) {
+System.out.println("Calculadora Empresa v1");
+}
+
+public static void suma(String[] args) {
 System.out.println("Función suma implementada");
+}
+
+public static void resta(String[] args) {
+System.out.println("Función resta implementada");
 }
 }
